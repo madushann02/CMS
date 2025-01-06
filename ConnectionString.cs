fbd0f5e7b7c03dced0772549022c645929f27562
madushann02 <cb011780@students.apiit.lk>
2025-01-06 11:55:49 +0530

    Add files via upload

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -8,6 +8,6 @@ namespace Madu
 {
     internal class ConnectionString
     {
-        public string Database = "server=localhost;port=3306;database=madu;user=root;password=212@shakeel;";
+        public string Database = "server=localhost;port=3306;database=madu;user=root;password=;";
     }
 }
